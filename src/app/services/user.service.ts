8d94615e5f281c502ab844e2d306eb7667cbe87f
emir57 <emir.gurbuz06@hotmail.com>
2022-01-06 20:32:29 +0300

    Add User model

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(
+    private fireStore:AngularFirestore
+  ) { }
+
+  addUser(){
+
+  }
 }
