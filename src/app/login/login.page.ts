0036fb7a22a1d4cc6d75c2cbe0d48cec2b8414c0
emir57 <emir.gurbuz06@hotmail.com>
2022-01-07 19:27:10 +0300

    Refactoring

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -5,6 +5,7 @@ import { AlertController } from '@ionic/angular';
 import { LoginModel } from '../models/loginModel';
 import { AuthService } from '../services/auth.service';
 import { MessageService } from '../services/message.service';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-login',
@@ -20,7 +21,8 @@ export class LoginPage implements OnInit {
     private router: Router,
     private messageService: MessageService,
     private authService: AuthService,
-    private alertController:AlertController
+    private alertController: AlertController,
+    private userService: UserService
   ) { }
 
   ngOnInit() {
@@ -36,17 +38,23 @@ export class LoginPage implements OnInit {
 
   login() {
     if (this.loginForm.valid) {
-      this.isOk=false;
+      this.isOk = false;
       let loginModel: LoginModel = this.loginForm.value;
-      this.authService.login(loginModel).then((msg)=>{
-        this.messageService.showMessage("Giriş Başarılı");
-        this.router.navigate(["home"])
-      }).catch(error=>{
+      this.authService.login(loginModel).then((msg) => {
+        this.userService.getUser(loginModel.email).subscribe(user => {
+          localStorage.setItem("user", JSON.stringify(user));
+        })
+        setTimeout(() => {
+          this.messageService.showMessage("Giriş Başarılı");
+          this.router.navigate(["home"])
+        }, 1300);
+
+      }).catch(error => {
         let message = this.messageService.GetErrorMessage(error.code);
         this.messageService.showMessage(message);
-      }).finally(()=>{
+      }).finally(() => {
         setTimeout(() => {
-          this.isOk=true;
+          this.isOk = true;
         }, 1000);
       })
     }
@@ -73,7 +81,7 @@ export class LoginPage implements OnInit {
         }, {
           text: 'Gönder',
           handler: (value) => {
-            this.authService.resetPassword(value.email).catch(error=>{
+            this.authService.resetPassword(value.email).catch(error => {
               let message = this.messageService.GetErrorMessage(error.code)
               this.messageService.showMessage(message);
             })
