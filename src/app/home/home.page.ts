b99af55ed2bc074e3f788e3d2f5107602a8c6d05
emir57 <emir.gurbuz06@hotmail.com>
2022-01-07 22:47:40 +0300

    Refactoring

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Message } from '../models/message';
+import { User } from '../models/user';
 import { ChatService } from '../services/chat.service';
 
 @Component({
@@ -7,8 +8,9 @@ import { ChatService } from '../services/chat.service';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
   list:string[]=[]
+  currentUser:User
   constructor(
     private chatService:ChatService
   ) {
@@ -21,6 +23,9 @@ export class HomePage {
     //   console.log(date)
     //   console.log(date.getDate()+" "+date.toLocaleDateString()+" "+date.toLocaleTimeString())
     // })
+  }
+  ngOnInit(): void {
+    this.currentUser = JSON.parse(localStorage.getItem("user"));
   }
 
 }
