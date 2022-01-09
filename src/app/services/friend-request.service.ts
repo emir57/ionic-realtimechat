ad3bbb9b71368366878fec5f978baf1ef6fa89ca
emir57 <emir.gurbuz06@hotmail.com>
2022-01-09 16:04:13 +0300

    Friend Request Service

--- src/app/services/friend-request.service.ts
+++ src/app/services/friend-request.service.ts
@@ -1,11 +1,37 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore, DocumentReference } from '@angular/fire/firestore';
+import { Observable, Subject } from 'rxjs';
+import { FriendRequestModel } from '../models/friendRequestModel';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FriendRequestService {
 
-  constructor() { }
+  collectionName="friendRequests";
+  constructor(
+    private fireStoreService:AngularFirestore
+  ) { }
 
+  add(friendRequestModel:FriendRequestModel){
+    return this.fireStoreService.collection(this.collectionName).add(friendRequestModel);
+  }
+  update(friendRequestModel:FriendRequestModel){
+    return this.fireStoreService.collection(this.collectionName).doc(friendRequestModel.id).update(friendRequestModel);
+  }
+  delete(friendRequestModel:FriendRequestModel){
+    return this.fireStoreService.collection(this.collectionName).doc(friendRequestModel.id).delete();
+  }
+  getRequests(currentUserEmail:string):Observable<FriendRequestModel[]>{
+    let subject = new Subject<FriendRequestModel[]>();
+    let returnRequests:FriendRequestModel[] = [];
+    this.fireStoreService.collection<FriendRequestModel>(this.collectionName).get().subscribe(requests=>{
+      requests.docs.forEach(request=>{
+        returnRequests.push(Object.assign({id:request.id},request.data()))
+      })
+      subject.next(returnRequests);
+    })
+    return subject.asObservable();
+  }
 
 }
