4c0bd487c9301a705fc7f5b9847d185a7ea026ca
emir57 <emir.gurbuz06@hotmail.com>
2022-01-09 17:36:14 +0300

    Add declarations and exports pipes

--- src/app/pipes/pipes/pipes.module.ts
+++ src/app/pipes/pipes/pipes.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SearchFriendPipe } from '../search-friend.pipe';
+import { SearchFriendRequestPipe } from '../search-friend-request.pipe';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [SearchFriendPipe,SearchFriendRequestPipe],
   imports: [
     CommonModule
-  ]
+  ],
+  exports:[SearchFriendPipe,SearchFriendRequestPipe]
 })
 export class PipesModule { }
