a525eb8b1bfc7cb6fd9578706f663e907d4bab7b
emir57 <emir.gurbuz06@hotmail.com>
2022-01-09 16:21:38 +0300

    Add List

--- src/app/friends-request/friends-request.page.ts
+++ src/app/friends-request/friends-request.page.ts
@@ -1,5 +1,9 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
+import { FriendRequestModel } from '../models/friendRequestModel';
+import { User } from '../models/user';
+import { FriendRequestService } from '../services/friend-request.service';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-friends-request',
@@ -9,12 +13,29 @@ import { ModalController } from '@ionic/angular';
 export class FriendsRequestPage implements OnInit {
   @Input() currentUserEmail:string;
 
+  friendRequestModel:FriendRequestModel;
+  users:User[]=[]
   constructor(
-    private modalController:ModalController
+    private modalController:ModalController,
+    private friendRequestService:FriendRequestService,
+    private userService:UserService
   ) { }
 
   ngOnInit() {
-    console.log(this.currentUserEmail)
+    this.friendRequestService.getRequests(this.currentUserEmail).subscribe(requests=>{
+      requests.forEach(request=>{
+        this.userService.getUser(request.receiveUserEmail).subscribe(user=>{
+          this.users.push(user);
+        })
+      })
+    })
+    // this.friendRequestModel = Object.assign({
+    //   senderUserEmail:this.currentUserEmail,
+    //   receiveUserEmail:"emir.gurbuz06@hotmail.com",
+    //   status:0
+    // });
+    // console.log(this.friendRequestModel)
+    // this.friendRequestService.add(this.friendRequestModel).then(()=>{});
   }
 
   close(){
