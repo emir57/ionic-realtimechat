4e22f6607b929b53e663945b41183ff3ea8fa0a6
emir57 <emir.gurbuz06@hotmail.com>
2022-01-09 17:37:34 +0300

    Configure SearchFriendRequestPipe

--- src/app/pipes/search-friend-request.pipe.ts
+++ src/app/pipes/search-friend-request.pipe.ts
@@ -1,11 +1,12 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { FriendRequestModel } from '../models/friendRequestModel';
 
 @Pipe({
   name: 'searchFriendRequest'
 })
 export class SearchFriendRequestPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
+  transform(value: FriendRequestModel[], ...args: unknown[]): FriendRequestModel[] {
     return null;
   }
 
