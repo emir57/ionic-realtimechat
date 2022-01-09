7383b0fa118663dbe1722cbd2fb082d7aaa914da
emir57 <emir.gurbuz06@hotmail.com>
2022-01-09 17:29:48 +0300

    Add Alert Controller

--- src/app/friends/friends.page.ts
+++ src/app/friends/friends.page.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { ModalController } from '@ionic/angular';
+import { AlertController, ModalController } from '@ionic/angular';
 import { FriendModel } from '../models/friendModel';
 import { FriendService } from '../services/friend.service';
 import { MessageService } from '../services/message.service';
@@ -11,37 +11,57 @@ import { UserService } from '../services/user.service';
   styleUrls: ['./friends.page.scss'],
 })
 export class FriendsPage implements OnInit {
-  @Input() currentUserEmail:string;
+  @Input() currentUserEmail: string;
 
-  friends:FriendModel[]=[];
+  friends: FriendModel[] = [];
   constructor(
-    private modalController:ModalController,
-    private friendService:FriendService,
-    private userService:UserService,
-    private messageService:MessageService
+    private modalController: ModalController,
+    private friendService: FriendService,
+    private userService: UserService,
+    private messageService: MessageService,
+    private alertController: AlertController
   ) { }
 
   ngOnInit() {
-    this.friendService.getFriends(this.currentUserEmail).subscribe(friends=>{
-      friends.forEach(friend=>{
-        this.userService.getUser(friend.friendUserEmail).subscribe(user=>{
-          this.friends.push(Object.assign({user:user},friend));
+    this.friendService.getFriends(this.currentUserEmail).subscribe(friends => {
+      friends.forEach(friend => {
+        this.userService.getUser(friend.friendUserEmail).subscribe(user => {
+          this.friends.push(Object.assign({ user: user }, friend));
         })
       })
     })
   }
 
 
-  close(){
+  close() {
     this.modalController.dismiss();
   }
 
-  remove(friend:FriendModel){
+  async remove(friend: FriendModel) {
+    const alert = await this.alertController.create({
+      header: 'Uyarı !',
+      message: `${friend.user.firstName} ${friend.user.lastName} kişisini arkadaşlıktan çıkarmak istediğinizden emin misiniz?`,
+      buttons: [
+        {
+          text: 'İptal',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: (blah) => {
+
+          }
+        }, {
+          text: 'Çık',
+          handler: () => {
+            let index = this.friends.findIndex(x => x.id === friend.id)
+            this.friends.splice(index, 1);
+          }
+        }
+      ]
+    });
+    await alert.present();
     // this.friendService.delete(friend).then(()=>{
     //   this.messageService.showMessage(`${friend.user.firstName} ${friend.user.lastName} başarıyla arkadaşlıktan çıakrtıldı`);
 
     // })
-    let index = this.friends.findIndex(x=>x.id===friend.id)
-    this.friends.splice(index,1);
   }
 }
