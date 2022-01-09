e9229e5818830b6e6796c4e89e9db0d9cbabc2cf
emir57 <emir.gurbuz06@hotmail.com>
2022-01-09 16:30:54 +0300

    Get Friends

--- src/app/services/friend.service.ts
+++ src/app/services/friend.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable, Subject } from 'rxjs';
 import { FriendModel } from '../models/friendModel';
+import { User } from '../models/user';
+import { UserService } from './user.service';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +12,8 @@ export class FriendService {
 
   private collectionName = "friends";
   constructor(
-    private fireStoreService:AngularFirestore
+    private fireStoreService:AngularFirestore,
+    private userService:UserService
   ) { }
 
   add(friendModel:FriendModel){
@@ -17,5 +21,20 @@ export class FriendService {
   }
   delete(friendModel:FriendModel){
     return this.fireStoreService.collection(this.collectionName).doc(friendModel.id).delete();
+  }
+  getFriends(currentUserEmail:string):Observable<User[]>{
+    let subject = new Subject<User[]>();
+    let users:User[]=[]
+    this.fireStoreService.collection<FriendModel>(this.collectionName).get().subscribe(doc=>{
+      doc.docs.forEach(doc=>{
+        if(doc.data().currentUserEmail === currentUserEmail){
+          this.userService.getUser(doc.data().friendUserEmail).subscribe(user=>{
+            users.push(user);
+          })
+        }
+      })
+      subject.next(users);
+    })
+    return subject.asObservable();
   }
 }
