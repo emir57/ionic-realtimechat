ad2ceeb2e1411e78722a4c0c7026ca9d92443dc9
emir57 <emir.gurbuz06@hotmail.com>
2022-01-13 18:17:58 +0300

    Group Service And Member Service

--- src/app/services/group.service.ts
+++ src/app/services/group.service.ts
@@ -1,24 +1,43 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable, Subject } from 'rxjs';
 import { GroupModel } from '../models/group';
+import { MemberService } from './member.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GroupService {
 
-  private collectionName="groups"
+  private collectionName = "groups"
   constructor(
-    private fireStoreService:AngularFirestore
+    private fireStoreService: AngularFirestore,
+    private memberService: MemberService
   ) { }
 
-  createGroup(groupModel:GroupModel){
+  createGroup(groupModel: GroupModel) {
     return this.fireStoreService.collection(this.collectionName).add(groupModel);
   }
-  deleteGroup(groupModel:GroupModel){
+  deleteGroup(groupModel: GroupModel) {
     return this.fireStoreService.collection(this.collectionName).doc(groupModel.id).delete();
   }
-  getGroups(userEmail:string){
-
+  getGroups(userEmail: string): Observable<GroupModel[]> {
+    let subject = new Subject<GroupModel[]>();
+    let returnValues: any[] = [];
+    this.memberService.getMembers().subscribe(members => {
+      members.forEach(member => {
+        if (member.userEmail === userEmail) {
+          this.fireStoreService.collection<GroupModel>(this.collectionName).get().subscribe(docs => {
+            docs.docs.forEach(doc => {
+              if (doc.data().id===member.groupId){
+                returnValues.push(Object.assign({id:doc.id},doc.data()));
+              }
+            })
+          })
+        }
+      })
+      return subject.next(returnValues);
+    })
+    return subject.asObservable();
   }
 }
--- src/app/services/member.service.ts
+++ src/app/services/member.service.ts
@@ -1,9 +1,47 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable, Subject } from 'rxjs';
+import { MemberModel } from '../models/member';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MemberService {
 
-  constructor() { }
+  private collectionName="members";
+  constructor(
+    private fireStoreService:AngularFirestore
+  ) { }
+
+  addMember(memberModel:MemberModel){
+    return this.fireStoreService.collection(this.collectionName).add(memberModel);
+  }
+  deleteMember(memberModel:MemberModel){
+    return this.fireStoreService.collection(this.collectionName).doc(memberModel.id).delete();
+  }
+  getMembers():Observable<MemberModel[]>{
+    let subject = new Subject<MemberModel[]>();
+    let returnValues:MemberModel[]=[];
+    this.fireStoreService.collection<MemberModel>(this.collectionName).get().subscribe(docs=>{
+      docs.docs.forEach(doc=>{
+        returnValues.push(Object.assign({id:doc.id},doc.data()));
+      })
+      return subject.next(returnValues);
+    })
+    return subject.asObservable();
+  }
+
+  getMembersByGroupId(groupId:string):Observable<MemberModel[]>{
+    let subject = new Subject<MemberModel[]>();
+    let returnValues:MemberModel[]=[];
+    this.fireStoreService.collection<MemberModel>(this.collectionName).get().subscribe(docs=>{
+      docs.docs.forEach(doc=>{
+        if(doc.data().groupId===groupId){
+          returnValues.push(Object.assign({id:doc.id},doc.data()));
+        }
+      })
+      return subject.next(returnValues);
+    })
+    return subject.asObservable();
+  }
 }
