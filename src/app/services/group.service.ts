676dc7d6e49bc58bc3e9da564236ae480ddcb41f
emir57 <emir.gurbuz06@hotmail.com>
2022-01-13 18:01:48 +0300

    Group Service

--- src/app/services/group.service.ts
+++ src/app/services/group.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { GroupModel } from '../models/group';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GroupService {
 
-  constructor() { }
+  private collectionName="groups"
+  constructor(
+    private fireStoreService:AngularFirestore
+  ) { }
+
+  createGroup(groupModel:GroupModel){
+    return this.fireStoreService.collection(this.collectionName).add(groupModel);
+  }
+  deleteGroup(groupModel:GroupModel){
+    return this.fireStoreService.collection(this.collectionName).doc(groupModel.id).delete();
+  }
+  getGroups(userEmail:string){
+
+  }
 }
