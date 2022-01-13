d684aeec1bf86a5e928e603bf67d5aad4e6c1ce2
emir57 <emir.gurbuz06@hotmail.com>
2022-01-13 15:42:59 +0300

    Friend Request Page Add Pipes Module

--- src/app/friends-request/friends-request.module.ts
+++ src/app/friends-request/friends-request.module.ts
@@ -7,12 +7,14 @@ import { IonicModule } from '@ionic/angular';
 import { FriendsRequestPageRoutingModule } from './friends-request-routing.module';
 
 import { FriendsRequestPage } from './friends-request.page';
+import { PipesModule } from '../pipes/pipes/pipes.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
+    PipesModule,
     FriendsRequestPageRoutingModule
   ],
   declarations: [FriendsRequestPage]
