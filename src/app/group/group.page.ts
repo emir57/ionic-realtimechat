765a6142374df71598cf0a2b72eaa56e1e394e10
emir57 <emir.gurbuz06@hotmail.com>
2022-01-24 16:15:05 +0300

    Get Messages

--- src/app/group/group.page.ts
+++ src/app/group/group.page.ts
@@ -1,7 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { GroupModel } from '../models/group';
-
+import { ChatService } from '../services/chat.service';
+import { Message } from "../models/message";
 @Component({
   selector: 'app-group',
   templateUrl: './group.page.html',
@@ -10,16 +11,30 @@ import { GroupModel } from '../models/group';
 export class GroupPage implements OnInit {
   @Input() group:GroupModel;
 
+  chats:Message[]=[]
   message:string="";
   constructor(
-    private modalController:ModalController
+    private modalController:ModalController,
+    private chatService:ChatService
   ) { }
 
   ngOnInit() {
-
+    console.log(this.group);
+    this.getMessages();
   }
   dismiss(){
     this.modalController.dismiss();
+  }
+
+  sendMessage(){
+
+  }
+
+  getMessages(){
+    this.chatService.getChatsByGroupId(this.group.id).subscribe(chats=>{
+      this.chats = chats;
+      console.log(chats)
+    })
   }
 
 
