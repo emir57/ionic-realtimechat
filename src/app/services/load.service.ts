78304c0c573d246ffa2af5edbc2eb1fc08e0bd19
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-05-15 18:03:24 +0300

    loadService showLoading and closeLoading functions

--- src/app/services/load.service.ts
+++ src/app/services/load.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { LoadingController } from '@ionic/angular';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoadService {
 
-  constructor() { }
+  constructor(
+    private loadingController: LoadingController
+  ) { }
+
+  async showLoading(message?: string) {
+    const loading = await this.loadingController.create({
+      message: message ?? "Yükleniyor...",
+      duration: 4000
+    });
+    return await loading.present();
+  }
+
+  async closeLoading(){
+    await this.loadingController.dismiss();
+  }
+
 }
