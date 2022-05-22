b23187d2f2669236627611d002caa60cf4c0111a
Emir Gürbüz <emir.gurbuz06@hotmail.com>
2022-05-22 16:26:32 +0300

    sweet service showSuccessMessage

--- src/app/services/sweet-alert.service.ts
+++ src/app/services/sweet-alert.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
-
+import { Swal } from "../../../src/sweetalert2.all.min";
 @Injectable({
   providedIn: 'root'
 })
 export class SweetAlertService {
 
   constructor() { }
+
+  showSuccessMessage() {
+    Swal.fire({
+      position: 'top-end',
+      icon: 'success',
+      title: 'Your work has been saved',
+      showConfirmButton: false,
+      timer: 1500
+    })
+  }
+
 }
